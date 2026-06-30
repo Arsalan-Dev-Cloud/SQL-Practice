59a66d533bba0c5dd6043aac149fbf647bedba30
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-06-30 13:39:35 +0530

    Add employee records to constraints.sql

---
+++ Day-03/constraints.sql
@@ -0,0 +1,8 @@
+INSERT INTO employees
+(emp_id, name, email, age, salary, department)
+VALUES
+(1, 'Arsalan', 'shaikhmohammadarsalan5@gmail.com', 23, 55000, 'IT'),
+(2, 'Shoaib', 'shoaibmaniyar@gmail.com', 22, 40000, 'HR'),
+(3, 'Faizan', 'sayyedfaizan@gmail.com', 21, 50000, 'Finance'),
+(4, 'Noor', 'saikhnoorooddin@gamil.com', 23, 60000, 'Marketing'),
+(5, 'Ali', 'alishaikh@gmail.com', 25, 70000, 'IT');
