44570a41139546f37f31e51967f3da6fa4dd0f2a
Shaikh Mohammad Arsalan Mohsin <shaikhmohammadarsalan5@gmail.com>
2026-07-01 17:43:31 +0530

    Fix comments in where_and_or.sql for clarity
    Corrected comments for clarity and consistency.

--- Day-04/where_and_or.sql
+++ Day-04/where_and_or.sql
@@ -8,7 +8,7 @@ CREATE TBALE employees(
   department VARCHAR(50)
 );
 
-#--------------use of Where Clause in Different terms----------------
+#--------------Use of Where Clause in Different terms----------------
 
 1)-->
 SELECT *                    
@@ -28,6 +28,50 @@ FROM employees
 WHERE age < 25;
 -->(here it shows the employees younger than 25)
 
+#------------------Use of And Operator----------------------
 
+1)-->
+SELECT *
+FROM employees
+WHERE department = 'IT'
+AND salary > 60000;
+--> It shows the employees in IT and earning more than 60,000.
+
+2)-->
+SELECT *
+FROM employees
+WHERE age > 22
+AND age < 28;
+--> It shows the employees older than 22 and younger than 28.
+
+#---------------Use of OR operator----------------
+
+1)-->
+SELECT *
+FROM employees
+WHERE department = 'IT'
+OR department = 'HR';
+--> It shows the employees in IT and HR.
+
+2)-->
+SELECT *
+FROM employees
+WHERE salary > 65000
+OR age < 23;
+--> It shows the employee salary greater than 65,000 or age less than 23.
+
+#--------------------Use of Not operator-----------------------
+
+1)-->
+SELECT *
+FROM employees
+WHERE NOT department = 'IT';
+--> It shows the employees who are not in IT.
+
+2)-->
+SELECT *
+FROM employees
+WHERE NOT salary > 50000;
+--> It shows the employees whose salary is not greater than 50,000.
 
 
